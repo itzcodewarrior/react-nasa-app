587fd8e7dc0ee6255382f98d1181811e4c2878a4
Tanmay Chaudhary <132979663+itzcodewarrior@users.noreply.github.com>
2024-08-21 18:55:38 +0530

    Add files via upload

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+function Main(props) {
+  const { data } = props;
+  return (
+    <div className="imgContainer">
+      <img
+        src={data?.hdurl}
+        alt={data?.title || "bg-img"}
+        className="bgImage"
+      />
+    </div>
+  );
+}
+
+export default Main;
